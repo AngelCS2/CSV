a70eaa0f48a4d5610ba0524379c2707cbb24020f
AngelCS2 <159652120+AngelCS2@users.noreply.github.com>
2024-02-10 11:35:24 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage(isYes) {
+    const messageContainer = document.getElementById('messageContainer');
+
+    if (isYes) {
+        messageContainer.innerText =  "Te iubescc mult draga mea iubii bubii,si aveam incredere ca vei accepta,sa stii ca iti multumesc pentru toate aceste sentimente nou create in mine pentru prima oara vreodata,nu am avut atata incredere in nimeni asa cum am in tine,uneori i might me too much dar te rog bear with me si totul se va rezolva Happy Valentine Day!!";
+    } else {
+        messageContainer.innerText = "Daca vezi asta inseamna ca te arde de glume,asa ca iubii bubii mea IESI SI APASA PE DA";
+    }
+}
